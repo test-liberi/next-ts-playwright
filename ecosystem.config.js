4f323d92976d858bd34544e55b8a7cf09961fe8c
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2024-05-21 14:07:52 +0200

    Update 2024-05-21T14:07:52

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
       {
         script: "node_modules/next/dist/bin/next",
         args: "start",
-        instances: "max",
         cwd: "./",
         env: {
           NODE_ENV: process.env.NODE_ENV || "production",
